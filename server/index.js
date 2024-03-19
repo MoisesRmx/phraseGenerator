8ae4994c73456d9331ad914b924620ad0ab74a74
Moises Roberto <moisesrobertosanchezperez@gmail.com>
2024-03-19 00:28:03 -0600

    Actualización 3

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js'
-import { config } from 'dotenv'
-
-config()
-
-const PORT = process.env.PH_PORT || 3000;
-
-app.listen(PORT)
-console.log('Running application on port: ', PORT)
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,9 +1,12 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.status(200).sendFile(`${process.cwd()}/app/index.html`)
+  res.status(200).send(`${process.cwd()}/app/index.html`)
 })
+
+
+
 
 export default router;
